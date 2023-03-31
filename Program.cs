f28a3e8b63ad22b8a0fc717e406cc7ac10439799
keemdonghoo <kiingdonghoo@gmail.com>
2023-03-31 13:04:51 +0900

    A#8 about

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@
             {
                 Console.WriteLine("헤딩입니다");  // A#2
                 Console.WriteLine("회원가입");   //B#4
+                Console.WriteLine("about");     //A#8
             }
             Console.WriteLine("article");
             {
